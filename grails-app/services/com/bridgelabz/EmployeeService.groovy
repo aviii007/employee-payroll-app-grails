acd83e1eeff9d3d1fa031bd59041ec5f685a1755
aviii007 <kanojia.avi@gmail.com>
2023-02-03 16:19:18 +0530

    Added: The save and list method for the employee

--- grails-app/services/com/bridgelabz/EmployeeService.groovy
+++ grails-app/services/com/bridgelabz/EmployeeService.groovy
@@ -1,11 +1,32 @@
 package com.bridgelabz
 
-import grails.gorm.transactions.Transactional
+import grails.web.servlet.mvc.GrailsParameterMap
 
-@Transactional
 class EmployeeService {
 
-    def serviceMethod() {
+     def save(GrailsParameterMap params) {
+        EmployeeModel employee = new EmployeeModel(params)
+        def response = AppUtil.saveResponse(false, member)
+        if (employee.validate()) {
+            employee.save(flush: true)
+            if (!employee.hasErrors()) {
+                response.isSuccess = true
+            }
+        }
+        return response
 
+    }
+
+    def list(GrailsParameterMap params) {
+        params.max = params.max ?: GlobalConfig.itemsPerPage()
+        List<EmployeeModel> employeeList = Employee.createCriteria().list(params) {
+            if (params?.colName && params?.colValue) {
+                like(params.colName, "%" + params.colValue + "%")
+            }
+            if (!params.sort) {
+                order("id", "desc")
+            }
+        } as List<EmployeeModel>
+        return [list: employeeList, count: employeeList.count()]
     }
 }
